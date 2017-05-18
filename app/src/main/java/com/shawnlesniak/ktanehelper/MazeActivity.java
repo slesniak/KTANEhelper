b3b79f387a5ff51d0f76829bd49a0a74f92f741d
Shawn Lesniak <moderkaka@gmail.com>
2017-05-17 22:02:01 -0700

    Initial commit

---
+++ app/src/main/java/com/shawnlesniak/ktanehelper/MazeActivity.java
@@ -0,0 +1,38 @@
+package com.shawnlesniak.ktanehelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MazeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maze);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
